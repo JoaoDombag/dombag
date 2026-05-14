430d371226c92585b8e0dac52559199eacde972e
João Garcia <joao@dombag.com.br>
2026-05-14 16:38:51 -0300

    deploy

---
+++ config/db.production.php
@@ -0,0 +1,13 @@
+<?php
+
+define('_DB_HOST', 'br1006.hostgator.com.br');
+define('_DB_PORT', '3306');
+define('_DB_NAME', 'joaofr15_sistemadombag');
+define('_DB_USER', 'joaofr15_sistemadombag');
+define('_DB_PASS', 'Dombag@12345');
+
+define('_PG_HOST',   'pg-yzidro-004.yzidro.com');
+define('_PG_PORT',   '44551');
+define('_PG_DBNAME', '004703_dom_bag_ltda');
+define('_PG_USER',   '004703consulta');
+define('_PG_PASS',   'Yz#2025Consulta');
